1b899011204fd1f5bf615c3b8b3d6ddefcb00f8f
[dyad] <git@dyad.sh>
2026-02-09 20:51:27 -0300

    [dyad] Correção do erro de importação do componente Badge - wrote 1 file(s)

--- src/pages/Departments.tsx
+++ src/pages/Departments.tsx
@@ -9,6 +9,7 @@ import {
   TableHeader, 
   TableRow 
 } from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Dialog,
